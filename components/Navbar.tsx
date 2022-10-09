ee690cc91934df00feba522edfd3cfefec6cbcbe
maj39623 <maj39623@gmail.com>
2022-10-09 12:46:10 -0400

    10/9

---
+++ components/Navbar.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { AiOutlineLogout } from "react-icons/ai";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import { BiSearch } from "react-icons/bi";
+import { IoMdAdd } from "react-icons/io";
+
+import Logo from "../utils/tiktik-logo.png";
+import { createOrGetUser } from "../utils";
+
+const Navbar = () => {
+  const user = false;
+
+  return (
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[130px]">
+          <Image
+            className="cursor-pointer"
+            src={Logo}
+            alt="TikTik"
+            layout="responsive"
+          />
+        </div>
+      </Link>
+
+      <div>Search</div>
+
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => createOrGetUser(response)}
+            onError={() => console.log("Error")}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
